c38d0317ff36bf234762224dc03151de0675744a
dawidj <dawidjanicki2@gmail.com>
2023-12-08 21:33:48 +0100

    Add country list

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { countries } from "./data";
+
+type countryType = {
+  name: string;
+  language: string;
+  area: number; // in km²
+  capital: string;
+  continent: string;
+  hasSeaAccess: boolean;
+  biggestCities: string[];
+};
+
+const country: countryType = {
+  name: "Australia",
+  language: "English",
+  area: 7692024, // in km²
+  capital: "Canberra",
+  continent: "Oceania",
+  hasSeaAccess: true,
+  biggestCities: ["Sydney", "Melbourne", "Brisbane"],
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Lista krajów</h1>
+      <div>
+        <h2>Dane kraju: </h2>
+        <li>Nazwa : {country.name}</li>
+        <li>Powierzchnia : {country.area}</li>
+        <li>Język : {country.language}</li>
+        <li>Stolica : {country.capital}</li>
+        <li>Dostęp do morza : {country.hasSeaAccess ? "tak" : "nie"}</li>
+        <li>Największe miasta: {country.biggestCities.join(", ")}</li>
+      </div>
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,72 @@
+export const countries =   [  {
+    name: "Brazil",
+    language: "Portuguese",
+    area: 8515767, // in km²
+    capital: "Brasília",
+    continent: "South America",
+    hasSeaAccess: true,
+    biggestCities: ["São Paulo", "Rio de Janeiro", "Brasília"]
+},
+{
+    name: "Canada",
+    language: "English, French",
+    area: 9984670, // in km²
+    capital: "Ottawa",
+    continent: "North America",
+    hasSeaAccess: true,
+    biggestCities: ["Toronto", "Montreal", "Vancouver"]
+},
+{
+    name: "China",
+    language: "Mandarin",
+    area: 9596961, // in km²
+    capital: "Beijing",
+    continent: "Asia",
+    hasSeaAccess: true,
+    biggestCities: ["Shanghai", "Beijing", "Chongqing"]
+},
+{
+    name: "Egypt",
+    language: "Arabic",
+    area: 1010408, // in km²
+    capital: "Cairo",
+    continent: "Africa",
+    hasSeaAccess: true,
+    biggestCities: ["Cairo", "Alexandria", "Giza"]
+},
+{
+    name: "France",
+    language: "French",
+    area: 643801, // in km²
+    capital: "Paris",
+    continent: "Europe",
+    hasSeaAccess: true,
+    biggestCities: ["Paris", "Marseille", "Lyon"]
+},
+{
+    name: "Germany",
+    language: "German",
+    area: 357386, // in km²
+    capital: "Berlin",
+    continent: "Europe",
+    hasSeaAccess: true,
+    biggestCities: ["Berlin", "Hamburg", "Munich"]
+},
+{
+    name: "India",
+    language: "Hindi, English",
+    area: 3287263, // in km²
+    capital: "New Delhi",
+    continent: "Asia",
+    hasSeaAccess: true,
+    biggestCities: ["Mumbai", "Delhi", "Bangalore"]
+},
+{
+    name: "Italy",
+    language: "Italian",
+    area: 301340, // in km²
+    capital: "Rome",
+    continent: "Europe",
+    hasSeaAccess: true,
+    biggestCities: ["Rome", "Milan", "Naples"]
+}]
